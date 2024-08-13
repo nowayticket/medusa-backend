deb8cc9543967ff6c699a9f4c3f8beb43131c3a4
nowayticket <gypsy8641@gmail.com>
2024-08-13 12:38:45 +0300

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -59,21 +59,6 @@ const plugins = [
         open: process.env.OPEN_BROWSER !== "false",
       },
     },
-  },
-  {
-    resolve: `medusa-plugin-sendgrid`,
-    options: {
-      api_key: process.env.SENDGRID_API_KEY,
-      from: process.env.SENDGRID_FROM,
-      order_placed_template: 
-        process.env.SENDGRID_ORDER_PLACED_ID,
-      localization: {
-        "de-DE": { // locale key
-          order_placed_template:
-            process.env.SENDGRID_ORDER_PLACED_ID_LOCALIZED,
-        },
-      },
-    },
   }
 ];
 
